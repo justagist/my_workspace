96964ccc7f90ff27ebcca98f08e2a1782a7aee5a
Saif Sidhik <saif.sidhik@dyson.com>
2024-03-05 09:48:00 +0000

    add base deps.yaml

--- README.md
+++ README.md
@@ -1,5 +1,8 @@
 # Workspace
 
-1. Clone any repo in this workspace with `deps.yaml`.
+1. Clone any repo in this workspace.
 2. Run `source ./setup_host_base.sh`. creates venv, installs rocker stuff, launches vscode in venv.
-3. Run vscode task `launch_container`.
+3. Run vscode task `launch_container`. All dependencies in files `deps.yaml` in this folder and all nested
+   directories will be installed as explained in [deps_rocker](https://github.com/blooop/deps_rocker) package readme.
+
+Uses [rocker](https://github.com/osrf/rocker) and extensions such as [deps_rocker](https://github.com/blooop/deps_rocker).
--- setup_scripts/launch_vscode.sh
+++ setup_scripts/launch_vscode.sh
@@ -10,14 +10,19 @@ git submodule update --init --recursive
 
 CONTAINER_NAME=${PWD##*/}  
 
+IMAGE_NAME="$CONTAINER_NAME:dev"
+
 echo "stopping existing container" "$CONTAINER_NAME" 
 docker stop "$CONTAINER_NAME" || true 
+docker kill "$CONTAINER_NAME" || true 
 
 CONTAINER_HEX=$(printf $CONTAINER_NAME | xxd -p | tr '\n' ' ' | sed 's/\\s//g' | tr -d ' ');
 
 export DEBIAN_FRONTEND=noninteractive
 
-rocker --nvidia --x11 --user --pull --network host --git --name "$CONTAINER_NAME" --volume "${PWD}":$HOME/workspaces/"${CONTAINER_NAME}":Z --oyr-run-arg " --detach" --deps-dependencies ros:humble
+rocker --nvidia --x11 --user --image-name "$IMAGE_NAME" --pull --network host --git --name "$CONTAINER_NAME" --volume "${PWD}":$HOME/workspaces/"${CONTAINER_NAME}":Z --oyr-run-arg " --detach" --deps-dependencies ros:humble
+
+# rocker --nvidia --x11 --user --git --name "$CONTAINER_NAME" --volume "${PWD}":$HOME/workspaces/"${CONTAINER_NAME}":Z --oyr-run-arg " --detach" $IMAGE_NAME
 
 # docker pull ghcr.io/red5d/docker-autocompose:latest
 # docker run --rm -v /var/run/docker.sock:/var/run/docker.sock ghcr.io/red5d/docker-autocompose $CONTAINER_NAME > .devcontainer/docker-compose.yaml
